ae91ecf6255f22d99dbeef428ba650e888d604b1
GiovanaNogueira <gigimnogueira@gmail.com>
2025-11-17 20:00:31 -0300

    feat: add IoT display functionality and new environment configuration

---
+++ src/tela.h
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void telaSetup();
+void telaInicial();
+void usuarioInvalido();
+void telaProdutos(String nomeUsuario, float saldoUsuario);
+void telaSegure();
+void telaSaldoFinal(float preco, float saldoUser);
+void telaSaldoNegativo(float saldoUser);
+void telaSemEstoque();
+int telaPesagem(float peso, float preco);
